73e2e169bd687beeea5c95bb39544bbf12721dbf
Stanislav Yudin <syudin@odin.com>
2017-01-14 04:06:49 +1100

    update tools, add start-dev dependent on entr(1) for auto rebuild

--- tools/create-db.sql
+++ tools/create-db.sql
@@ -7,5 +7,14 @@ create table session (
 	expire_on	timestamp not null
 );
 
+create table item (
+	client		varchar(36) references session (client),
+	key			varchar(255),
+	str_val		varchar(1024),
+	json_val	json,
+	blob_val	bytea
+);
+
 create user servo with password 'test';
 grant all privileges on table session to servo;
+grant all privileges on table item to servo;
