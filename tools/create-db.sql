7f6866b38d53f104c0ceac219fbb5c7fae5bd7f9
Stanislav Yudin <stan@endlessinsomnia.com>
2017-03-28 13:36:33 +1100

    change db schema to keep track to last read and write

--- tools/create-db.sql
+++ tools/create-db.sql
@@ -2,19 +2,25 @@ create database servodb encoding 'UTF8';
 
 \connect servodb;
 
-create table session (
-	client		varchar(36) primary key,
-	expire_on	timestamp not null
-);
-
 create table item (
-	key			varchar(255) primary key,
-	client		varchar(36) references session (client),
+	key			varchar(255),
+	client		varchar(36),
+	last_read	timestamp not null,
+	last_write	timestamp not null,
 	str_val		text,
 	json_val	json,
-	blob_val	bytea
+	blob_val	bytea,
+	primary key(key, client)
 );
 
+create function servo_get_item(c varchar(36), k varchar(255))
+	returns table(str_val text, json_val json, blob_val bytea) as $$
+begin
+	update item i set last_read = now() where i.key = k and i.client = c;
+	return query select i.str_val, i.json_val, i.blob_val from item i where i.key = k and i.client = c;
+end;
+$$ language plpgsql;
+
+
 create user servo with password 'test';
-grant all privileges on table session to servo;
 grant all privileges on table item to servo;
