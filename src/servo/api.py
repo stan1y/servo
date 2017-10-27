bc62350fe9df34a968ee0147f80724dfca1b2849
Stanislav Yudin <stan@endlessinsomnia.com>
2017-10-27 12:33:46 +1100

    style fixes

--- src/servo/api.py
+++ src/servo/api.py
@@ -9,9 +9,18 @@ import servo.db
 log = logging.getLogger(__name__)
 
 
+async def response(req, status, sbody):
+    return aiohttp.web.Response(
+        status=status,
+        content_type=req['context']['out_type'],
+        body=sbody.encode(req.charset))
+
+
 @servo.auth.authenticate
 async def get(req):
-    return aiohttp.web.json_response(await servo.db.get(req))
+    return aiohttp.web.Response(
+        content_type=req['context']['out_type'],
+        body=await servo.db.get(req))
 
 
 @servo.auth.authenticate
@@ -20,8 +29,10 @@ async def post(req):
         log.error('{%s} POST has no body' % req['context']['token']['id'])
         raise aiohttp.web.HTTPBadRequest()
     await servo.db.post(req)
-    return aiohttp.web.json_response(await servo.db.get(req),
-                                     status=201)
+    return aiohttp.web.Response(
+        content_type=req['context']['out_type'],
+        body=await servo.db.get(req),
+        status=201)
 
 
 @servo.auth.authenticate
@@ -30,7 +41,9 @@ async def put(req):
         log.error('{%s} PUT has no body' % req['context']['token']['id'])
         raise aiohttp.web.HTTPBadRequest()
     await servo.db.put(req)
-    return aiohttp.web.json_response(await servo.db.get(req))
+    return aiohttp.web.Response(
+        content_type=req['context']['out_type'],
+        body=await servo.db.get(req))
 
 
 @servo.auth.authenticate
